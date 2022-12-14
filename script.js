ca9c715b6b8ed47166740681d268e2070b701130
tobias-ahlund <tobias.ahlund@gmail.com>
2022-12-14 14:16:56 +0100

    some changes

--- script.js
+++ script.js
@@ -15,16 +15,24 @@ const founderNameMobile = document.querySelector(".founder-section-p-name");
 
 const founderNameDesktop = document.querySelector(".founder-name");
 
+const mobileHero = document.querySelector(".hero");
+
+const desktopHero = document.querySelector(".hero-desktop");
+
 var w = window.innerWidth
 
 if (w <= 767) {
     founderNameMobile.classList.add("visible");
+    mobileHero.classList.add("visible");
 } else if (w >= 768) {
     founderNameMobile.classList.add("hidden");
+    mobileHero.classList.add("hidden");
 }
 
 if (w <= 767) {
     founderNameDesktop.classList.add("hidden");
+    desktopHero.classList.add("hidden");
 } else if (w >= 768) {
     founderNameDesktop.classList.add("visible");
+    desktopHero.classList.add("visible");
 }
